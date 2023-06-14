b9f5e1e4d8d45462b6a1278768b9e797f1167467
Ergys Hila <ergysmania@gmail.com>
2023-06-14 09:52:37 -0400

    edit

---
+++ Section 9/Coding Exercise 15.py
@@ -0,0 +1,8 @@
+def check_same_frequency(list1, list2):
+    def count_elements(lst):
+        counter = {}
+        for element in lst:
+            counter[element] = counter.get(element, 0) + 1
+        return counter
+    
+    return count_elements(list1) == count_elements(list2)
