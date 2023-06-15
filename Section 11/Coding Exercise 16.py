606b53d81b364dd241c404988bf3bfe8374a49e6
Ergys Hila <ergysmania@gmail.com>
2023-06-15 08:52:45 -0400

    edit

---
+++ Section 11/Coding Exercise 16.py
@@ -0,0 +1,9 @@
+def sum_product(t):
+    sum_result = 0
+    product_result = 1
+ 
+    for num in t:
+        sum_result += num
+        product_result *= num
+ 
+    return sum_result, product_result
