0f9f70717f44f892856f7006ba6c954bfa03db48
Ergys Hila <ergysmania@gmail.com>
2023-06-16 10:19:48 -0400

    CSLL

---
+++ Section 15/CSLL.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+ 
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+ 
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
+
+
+    def createCSLL(self, value):
+        node = Node(value=value)
+        node.next = node
+        node.head = node
+        node.tail = node
