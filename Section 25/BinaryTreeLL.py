5f9f3c36a1143437e6642a2859e7c152ee63d742
Ergys Hila <ergysmania@gmail.com>
2023-07-19 16:29:10 -0400

    Level Order

--- Section 25/BinaryTreeLL.py
+++ Section 25/BinaryTreeLL.py
@@ -8,6 +8,10 @@ class TreeNode:
 
 newBT = TreeNode("Drinks")
 leftChild = TreeNode("Hot")
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+leftChild.leftChild = tea
+leftChild.rightChild = coffee
 rightChild = TreeNode("Cold")
 newBT.leftChild = leftChild
 newBT.rightChild = rightChild
@@ -16,7 +20,7 @@ newBT.rightChild = rightChild
 def preOrderTraversal(rootNode):
     if not rootNode:
         return
-    print(rootNode.data)
+    print(rootNode.data +'Pre')
     preOrderTraversal(rootNode.leftChild)
     preOrderTraversal(rootNode.rightChild)
 
@@ -26,9 +30,15 @@ def inOrderTraversal(rootNode):
     if not rootNode:
         return
     inOrderTraversal(rootNode.leftChild)
-    print(rootNode.data)
+    print(rootNode.data +"IN")
     inOrderTraversal(rootNode.rightChild)
 
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data + "Post")
 
 
 
@@ -41,3 +51,4 @@ def inOrderTraversal(rootNode):
 
 preOrderTraversal(newBT)
 inOrderTraversal(newBT)
+postOrderTraversal(newBT)
