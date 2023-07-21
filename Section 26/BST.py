dbfff74eb3045df6d64a5823cb87fa1fd57ce35b
Ergys Hila <ergysmania@gmail.com>
2023-07-21 10:24:55 -0400

    BST

---
+++ Section 26/BST.py
@@ -0,0 +1,16 @@
+class BSTNode:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+
+
+
+
+
+    
+
+
+
+newBST = BSTNode(None)
