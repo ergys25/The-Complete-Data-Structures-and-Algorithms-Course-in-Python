53c582eb916dcd3cae7740d90c191082852cb03f
Ergys Hila <ergysmania@gmail.com>
2023-07-21 10:35:42 -0400

    Insert

--- Section 26/BST.py
+++ Section 26/BST.py
@@ -9,8 +9,27 @@ class BSTNode:
 
 
 
-    
+def insert(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insert(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue)
+        else:
+            insert(rootNode.rightChild, nodeValue)
+    return "Inserted"
+
 
 
 
 newBST = BSTNode(None)
+insert(newBST, 50)
+insert(newBST, 20)
+insert(newBST, 30)
+insert(newBST, 60)
+insert(newBST, 70)
