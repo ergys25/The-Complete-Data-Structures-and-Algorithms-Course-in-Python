ac265c45a30e8df527977f3d9b96608519e3a2f0
Ergys Hila <ergysmania@gmail.com>
2023-07-21 11:14:13 -0400

    traversalls

--- Section 26/BST.py
+++ Section 26/BST.py
@@ -24,12 +24,52 @@ def insert(rootNode, nodeValue):
             insert(rootNode.rightChild, nodeValue)
     return "Inserted"
 
+def preOrderTraversall(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversall(rootNode.leftChild)
+    preOrderTraversall(rootNode.rightChild)
 
+def inOrderTraversall(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversall(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversall(rootNode.rightChild)
+
+def postOrderTraversall(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversall(rootNode.leftChild)
+    print(rootNode.data)
+    postOrderTraversall(rootNode.rightChild)
+
+def levelOrdertraversall(rootNode):
+    if not rootNode:
+        return
+    stack = []
+    stack.append(rootNode)
+    while not (len(stack) == 0):
+        root = stack.pop()
+        print(root.data)
+        if root.leftChild is not None:
+            stack.append(root.leftChild)
+        if root.rightChild is not None:
+            stack.append(root.rightChild)
 
 
 newBST = BSTNode(None)
+insert(newBST, 70)
 insert(newBST, 50)
-insert(newBST, 20)
+insert(newBST, 90)
 insert(newBST, 30)
 insert(newBST, 60)
-insert(newBST, 70)
+insert(newBST, 80)
+insert(newBST, 100)
+insert(newBST, 20)
+insert(newBST, 40)
+
+preOrderTraversall(newBST)
+inOrderTraversall(newBST)
+postOrderTraversall(newBST)
