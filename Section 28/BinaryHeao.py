768b42fbb4311c90f9c9db9482f035bcc3e7da5c
Ergys Hila <ergysmania@gmail.com>
2023-07-25 16:00:55 -0400

    Creation

---
+++ Section 28/BinaryHeao.py
@@ -0,0 +1,10 @@
+class Heap:
+     def __init__(self,size) -> None:
+          self.customList = (size + 1) * None
+          self.heapSize = 0
+          self.maxSize = size + 1
+
+
+newBH = Heap(5)
+
+
