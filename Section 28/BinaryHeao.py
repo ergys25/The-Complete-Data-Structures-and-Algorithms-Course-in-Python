40cb5b5c05d57b2f278b8ef9e01fead95a364b09
Ergys Hila <ergysmania@gmail.com>
2023-07-25 16:10:27 -0400

    size

--- Section 28/BinaryHeao.py
+++ Section 28/BinaryHeao.py
@@ -1,15 +1,21 @@
 class Heap:
      def __init__(self,size) -> None:
-          self.customList = (size + 1) * None
+          self.customList = (size + 1) * [None]
           self.heapSize = 0
           self.maxSize = size + 1
 
 def peek(rootNode):
-     if not rootNode:
-          return
-     else:
-          return rootNode.customList[1]
+    if not rootNode:
+        return
+    else:
+        return rootNode.customList[1]
+        
+def size(rootNode):
+    if not rootNode:
+        return
+    else:
+         return rootNode.heapSize
 
 newBH = Heap(5)
-
+print(size(newBH))
 
