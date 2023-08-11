4521199117b1119975ef4544f66f883c6ad84525
Ergys Hila <ergysmania@gmail.com>
2023-08-11 09:15:21 -0400

    sorting

---
+++ Section 32/Insertion Sort.py
@@ -0,0 +1,14 @@
+def insertionSort(custom_list):
+    for i in range(1, len(custom_list)):
+        key = custom_list[i]
+        j = i - 1
+        while j>=0 and key < custom_list[j]:
+            custom_list[j+1] = custom_list[j]
+            j -= 1
+        custom_list[j+1] = key
+    print(custom_list)
+
+
+
+cl = [2,3,5,4,1,7,6,9,8]
+insertionSort(cl)
