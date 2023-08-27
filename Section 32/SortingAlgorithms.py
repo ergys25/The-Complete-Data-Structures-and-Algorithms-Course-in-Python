f5d7501cc8eee7353d7795740f1adbd94935a5b3
Ergys Hila <ergysmania@gmail.com>
2023-08-26 21:39:44 -0400

    edit

--- Section 32/SortingAlgorithms.py
+++ Section 32/SortingAlgorithms.py
@@ -7,7 +7,7 @@ def bubbleSort(customList):
         for j in range(len(customList)-i-1):
             if customList[j] > customList[j+1]:
                 customList[j], customList[j+1] = customList[j+1], customList[j]
-    print(customList)
+    print( customList)
 
 
 def selectionSort(customList):
