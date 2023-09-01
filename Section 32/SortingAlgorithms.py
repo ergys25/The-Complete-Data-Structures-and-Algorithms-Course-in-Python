85fa732c10f124011c683120dc9b896e26726541
Ergys Hila <ergysmania@gmail.com>
2023-09-01 14:12:50 -0400

    edit

--- Section 32/SortingAlgorithms.py
+++ Section 32/SortingAlgorithms.py
@@ -83,7 +83,7 @@ def merge( customList, l, m, r ):
     while j < n2:
         customList[k] = R[j]
         j += 1
-        k += 1
+        k+= 1
 
 def mergeSort( customList, l, r ):
     if l < r:
